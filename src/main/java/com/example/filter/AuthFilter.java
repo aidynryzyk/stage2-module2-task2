96c2cb5e7c36abd24355736d3901f625e1cd6b86
aidyninho <ryzyk.aidyn@gmail.com>
2023-04-05 02:13:33 +0600

    Added servltes, filters, listeners

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,20 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/user/*")
+public class AuthFilter extends HttpFilter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if (req.getAttribute("user") == null) {
+            req.getRequestDispatcher("/login.jsp").forward(req, res);
+        }
+    }
 }
